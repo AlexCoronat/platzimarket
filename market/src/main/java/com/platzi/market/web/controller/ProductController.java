850f86d82ba895c6c0d93243c61dedf1d052c9e1
Alex Corona <aacoroona@gmail.com>
2023-02-09 23:13:20 -0600

    Correccion en las credenciales de la base de datos

--- market/src/main/java/com/platzi/market/web/controller/ProductController.java
+++ market/src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.platzi.market.web.controller;
 import com.platzi.market.domain.Product;
 import com.platzi.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +16,12 @@ import java.util.Optional;
 public class ProductController {
     @Autowired /*Es posible utilizar esta anotacion porque ProductService tiene una anotacion de Spring*/
     private ProductService productService;
+    @GetMapping("/all")
     public List<Product> getAll(){
         return productService.getAll();
     }
-    public Optional<Product> getProduct(int productId){
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int productId){
         return productService.getProduct(productId);
     }
     public Optional<List<Product>> getByCategory(int categoryId){
