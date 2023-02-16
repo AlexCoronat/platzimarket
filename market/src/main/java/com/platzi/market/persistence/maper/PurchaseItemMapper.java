95b05a94ff0196c8921cbe6ee6a532692d2e6925
Alex Corona <aacoroona@gmail.com>
2023-02-16 00:32:41 -0600

    Creacion de Purchase

---
+++ market/src/main/java/com/platzi/market/persistence/maper/PurchaseItemMapper.java
@@ -0,0 +1,29 @@
+package com.platzi.market.persistence.maper;
+
+import com.platzi.market.domain.Purchaseitem;
+import com.platzi.market.persistence.entity.ComprasProducto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring", uses = {ProductMapper.class})
+/*Mapper utiliza internamente ProductMapper solo para ignorar los
+targets, pero al tratarse de una interface se tienen que tomat en
+cuenta
+ */
+public interface PurchaseItemMapper {
+    @Mappings({
+            @Mapping(source = "id.idProducto", target = "productId"),
+            @Mapping(source = "cantidad", target = "quantity"),
+            @Mapping(source = "estado", target = "active")
+    })
+    Purchaseitem toPurchaseItem(ComprasProducto producto);
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "compra", ignore = true),
+            @Mapping(target = "producto", ignore = true),
+            @Mapping(target = "id.idCompra", ignore = true)
+    })
+    ComprasProducto toComprasProducto(Purchaseitem item);
+}
